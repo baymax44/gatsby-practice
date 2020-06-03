6f6c156d8c51d4792ed0962027bc3afc6eb220d6
baymax44 <smartbay2021@outlook.com>
2020-06-02 22:34:24 -0400

    navbar & links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <section>
+      <Navbar />
       <div>
         <h1>This is homepage</h1>
       </div>
