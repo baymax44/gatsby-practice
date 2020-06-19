425081ffe1e654592f76c510f0c619b64477ca62
baymax44 <smartbay2021@outlook.com>
2020-06-19 06:27:07 -0400

    optimized image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,9 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from '../components/Layout'
 import * as styles from '../styles/home.module.css'
+import Img from "gatsby-image"
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <Layout>
       <section className={styles.header}>
@@ -13,8 +14,20 @@ export default function Home() {
           <p>UX designer & web developer based in US</p>
           <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
-        <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
+        <Img fluid={data.file.childImageSharp.fluid} />
       </section>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query Banner {
+    file(relativePath: {eq: "banner.png"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
